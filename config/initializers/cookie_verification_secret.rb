fe95466f5eb4876421ab3653cc3f687b5445e396
Aleo Nimes <aleo.nimes@gmail.com>
2010-06-23 00:48:31 +0200

    Initial commit

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = 'b27caa88acba2ded303822b050ea250bde9171c4a531c0f1d2eda877e4bbfdfb6bcbfd7cfccde40143d62663e575053b2b6af1e54c9011d7fd61135c527a1924';
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,15 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying cookie session data integrity.
+# If you change this key, all old sessions will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.session = {
+  :key         => '_sample_app_session',
+  :secret      => '85db802a6ce2131e511bdc29b16ca5756df814a79715661a254cb80c51fa98dc1192c179439cb7d42f6885cede886f6a759c2d5b21fce1d5778850827834d207'
+}
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# ActionController::Base.session_store = :active_record_store
